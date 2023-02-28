32e1a399d0379438a5a45604eb8ce5605f970ce7
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2023-02-28 22:20:02 +0200

    Task-3

--- string.js
+++ string.js
@@ -15,4 +15,26 @@ const reverseString = (str) => {
   return str.split('').reverse().join('');
 }
 
-  module.exports = { strlength, reverseString };
+// Task 3
+
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    if (b === 0) {
+      throw new Error('Cannot divide by zero');
+    }
+    return a / b;
+  }
+}
+module.exports = { strlength, reverseString, Calculator };
--- string.test.js
+++ string.test.js
@@ -1,4 +1,4 @@
-const { strlength, reverseString } = require('./string')
+const { strlength, reverseString, Calculator } = require('./string')
 // Task 1
 test('char count', () => {
   expect(strlength('mostafa')).toBe(7)
@@ -16,3 +16,61 @@ test('check str more than ten', () => {
 test('reverse string', () => {
   expect(reverseString('hello')).toBe('olleh')
 })
+
+// Task 3
+
+test('adds positive numbers', () => {
+  expect(new Calculator().add(2, 3)).toBe(5);
+});
+
+test('adds negative numbers', () => {
+  expect(new Calculator().add(-2, -3)).toBe(-5);
+});
+
+test('adds a positive and a negative number', () => {
+  expect(new Calculator().add(2, -3)).toBe(-1);
+});
+
+
+test('subtracts positive numbers', () => {
+  expect(new Calculator().subtract(3, 2)).toBe(1);
+});
+
+test('subtracts negative numbers', () => {
+  expect(new Calculator().subtract(-3, -2)).toBe(-1);
+});
+
+test('subtracts a negative from a positive number', () => {
+  expect(new Calculator().subtract(3, -2)).toBe(5);
+});
+
+
+test('multiplies positive numbers', () => {
+  expect(new Calculator().multiply(2, 3)).toBe(6);
+});
+
+test('multiplies negative numbers', () => {
+  expect(new Calculator().multiply(-2, -3)).toBe(6);
+});
+
+test('multiplies a positive and a negative number', () => {
+  expect(new Calculator().multiply(2, -3)).toBe(-6);
+});
+
+
+
+test('divides positive numbers', () => {
+  expect(new Calculator().divide(6, 3)).toBe(2);
+});
+
+test('divides negative numbers', () => {
+  expect(new Calculator().divide(-6, -3)).toBe(2);
+});
+
+test('divides a positive by a negative number', () => {
+  expect(new Calculator().divide(6, -3)).toBe(-2);
+});
+
+test('throws an error when dividing by zero', () => {
+  expect(() => new Calculator().divide(6, 0)).toThrow(Error);
+});
